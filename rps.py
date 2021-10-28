b64a09cc6958813fb517515f50a390ef98fff213
Laura Kovačić <laurakciic@gmail.com>
2021-10-28 20:23:13 +0200

    add unit_tests and run_tests, minor modification to function winner

--- rps.py
+++ rps.py
@@ -1,21 +1,21 @@
 def winner(player1, player2):
     if player1 == player2:
-        return("Both choices are same!")
-    elif player1 == 'rock':
-        if player2 == 'scissors':
-            return("Player1 wins!")
-        if player2 == 'paper':
-            return("Player2 wins!")
-    elif player1 == 'scissors':
-        if player2 == 'rock':
-            return("Player2 wins!")
-        if player2 == 'paper':
-            return("Player2 wins!")
-    elif player1 == 'paper':
-        if player2 == 'rock':
-            return("Player1 wins")
-        if player2 == 'scissors':
-            return("Player2 wins!")
+        return("draw")
+    elif player1 == "rock":
+        if player2 == "scissors":
+            return("player1")
+        if player2 == "paper":
+            return("player2")
+    elif player1 == "scissors":
+        if player2 == "rock":
+            return("player2")
+        if player2 == "paper":
+            return("player1")
+    elif player1 == "paper":
+        if player2 == "rock":
+            return("player1")
+        if player2 == "scissors":
+            return("player2")
     return None
 
 def check_inputs(p1, p2):
@@ -25,6 +25,7 @@ def check_inputs(p1, p2):
     else:
         return False
 
+
 def main():
     while True:
         player1 = input("Player1, enter your choice: ")
@@ -32,7 +33,7 @@ def main():
         if check_inputs(player1, player2):
             result = winner(player1, player2)
         else:
-            print("Wrong typo, try again.")
+            print("Wrong typo, try again ")
             continue
         
         print(result)
@@ -41,5 +42,23 @@ def main():
         if resume == "no":
             break
 
+def run_test(player1, player2, expected):
+    print(player1, "vs.", player2)
+    result = winner(player1, player2)
+    assert result == expected
+    print("PASS")
+
+def unit_tests():
+    run_test("rock", "paper", "player2")
+    run_test("rock", "scissors", "player1")
+    run_test("rock", "rock", "draw")
+    run_test("paper", "paper", "draw")
+    run_test("paper", "rock", "player1")
+    run_test("paper", "scissors", "player2")
+    run_test("scissors", "scissors", "draw")
+    run_test("scissors", "rock", "player2")
+    run_test("scissors", "paper", "player1")
+
 if __name__ == "__main__":
-    main()
+    # main()
+    unit_tests()
